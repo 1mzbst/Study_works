ada3216dce2f4d8bef0938b609a020466e148beb
Dmitriy Kulakov <dk4git@outlook.com>
2023-04-08 22:53:51 +0300

    StringArr Meth

--- Final_papers/Paper1/Program.cs
+++ Final_papers/Paper1/Program.cs
@@ -3,3 +3,17 @@
 // длина которых меньше или равна 3 символа. 
 
 //Решение:
+
+string[] CreateStringArr(int size) // Метод создание массива состоящего из строк.
+{
+    string[] array = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine(); // Ввод осуществляется через консоль, вручную.
+        if (array[i] == "") // Условие, если пользователь пропустил ввод слова.
+        {
+            array[i] = "empty_index";
+        }
+    }
+    return array;
+}
